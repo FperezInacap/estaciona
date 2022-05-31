07212b45bd6aabc612dd108d7308d2426b540542
Francisco <francisco.perez48@inacapmail.cl>
2022-05-31 18:15:34 -0400

    intento 10 de geolocalización

--- my-project/myapp/app/Views/reserva/geolocalizacion.php
+++ my-project/myapp/app/Views/reserva/geolocalizacion.php
@@ -1,27 +1,83 @@
-<?= $this->extend('/login/formausuario') ?>
-
-<?= $this->section('contenido') ?>
 <!DOCTYPE html>
-<html>
-<body>
-
-<h1>Mapa estaciona</h1>
-
-<div align="center" id="googleMap" style="width:50%;height:400px;align:center"></div>
-
-<script>
-function myMap() {
-var mapProp= {
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mapa Estaciona</title>
+  <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+  <script defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAw6WsNzg_29tKLgEulinYe-16IJ2dI2K0&callback=initMap"></script>
   
-  center:new google.maps.LatLng(-33.4051476,-70.6827313),
-  zoom:15,
-};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+ <script>
+      var map;
+      var marker;
+      var WatchID;
+      var geoLoc;
+
+      function initMap(){
+        const myLatLng =  {lat: -25.363, lng:  131.044};
+        map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 4,
+            center: myLatLng,
+         });
+         marker = new google.maps.Marker({
+           position: myLatLng,
+           map,
+           title:"Hola Mundo"
+         });  
+         getPosition()
+      }
+
+      function getPosition(){
+    if (navigator.geolocation) {
+        var options= {timeout:60000};
+        geoLoc = navigator.geolocation;
+        watchId =geoLoc.watchPosition(showLocationOnMap, errorHandler,options);
+
+}else{
+  alert("No soporta Geolocalizacion");
 }
-</script>
+}
+ 
+    function showLocationOnMap(position){
+        var latitud = position.coords.latitude;
+        var longitud = position.coords.longitude;
+        console.log(latitud +longitud);
+        const myLatLng = {lat: latitud, lang: longitud};
+        marker.setPosition(myLatLng);
+        map.setCenter(myLatLng);
 
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAw6WsNzg_29tKLgEulinYe-16IJ2dI2K0&callback=myMap"></script>
+        
 
-<?= $this->endSection() ?>
+    }
+
+    function errorHandler(err){
+        if(err.code == 1){
+        alert("Error: acceso denegado.");
+        } else if(err.code==2){
+          alert("error posision no se encuentra");
+
+        }
+
+    }
+ 
+ 
+    </script>
+ <style type="text/css">
+#map {
+  height: 100%;
+}
+
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
+  </head>
+  <body>
+    <div id="map"></div>
+     
 </body>
-</html>   
+</html>
